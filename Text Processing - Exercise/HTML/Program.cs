5ec65c3e8739a612000b7f85cc53bb07bd94cb33
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-16 18:26:54 +0200

    Add task

---
+++ Text Processing - Exercise/HTML/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            PrintTitle(title);
+            PrintContent(content);
+
+            string comments = Console.ReadLine();
+
+            while (comments != "end of comments")
+            {
+                PrintComments(comments);
+
+                comments = Console.ReadLine();
+            }
+        }
+
+        public static void PrintTitle(string title)
+        {
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+        }
+
+        public static void PrintContent(string content)
+        {
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {content}");
+            Console.WriteLine("</article>");
+        }
+
+        public static void PrintComments(string comments)
+        {
+            Console.WriteLine("<div>");
+            Console.WriteLine($"    {comments}");
+            Console.WriteLine("</div>");
+        }
+    }
+}
