f8a9b7753a5a90857155838055690c9d28784c41
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-16 16:06:15 +0200

    Add task

---
+++ Text Processing - Exercise/Ascii Sumator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string randomText = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < randomText.Length; i++)
+            {
+                if (randomText[i] > firstChar && randomText[i] < secondChar)
+                {
+                    sum += randomText[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
