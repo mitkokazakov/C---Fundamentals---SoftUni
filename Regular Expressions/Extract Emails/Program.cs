47fc9145ef8ad87f0f7118ca103ba2f2a2b791b6
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-17 21:44:26 +0200

    Add first part working at 50%

---
+++ Regular Expressions/Extract Emails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"[A-Za-z.-0-9_]+?[A-Za-z0-9]+[_\-.]?@[A-Za-z]+[-.]*?\.?[A-Za-z]+\.[A-Za-z]+";
+
+            Regex regex = new Regex(pattern);
+
+            var extractedEmails = regex.Matches(text);
+
+            foreach (Match mail in extractedEmails)
+            {
+                Console.WriteLine(mail.Value);
+            }
+        }
+    }
+}
