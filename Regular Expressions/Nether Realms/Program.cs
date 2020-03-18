45bc64aff7b2cf85ce16b589ceda3b038e68d41e
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-18 17:18:55 +0200

    Ne tasks added

---
+++ Regular Expressions/Nether Realms/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] {' ', ',' ,'\t', '\n' },StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, List<double>> demons = new Dictionary<string, List<double>>();
+
+            //Console.WriteLine(String.Join(Environment.NewLine,input));
+
+            string patternHealth = @"[^0-9.+\/*-]";
+            //[+-]?[0-9]\.?[0-9]?
+            string patternDamage = @"([-|+]?[0-9\.]*[0-9])";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentName = input[i];
+
+                //if (currentName.Contains(' ') || currentName.Contains(','))
+                //{
+                //    continue;
+                //}
+
+                Regex health = new Regex(patternHealth);
+                var symbolsForHealth = health.Matches(currentName);
+                int sumHealth = 0;
+
+                foreach (Match letter in symbolsForHealth)
+                {
+                    sumHealth += Convert.ToChar(letter.Value);
+                }
+
+                Regex damage = new Regex(patternDamage);
+                var symbolsForDamage = damage.Matches(currentName);
+                double sumDamage = 0;
+
+                foreach (Match digit in symbolsForDamage)
+                {
+                    sumDamage += double.Parse(digit.Value);
+                }
+
+                for (int j = 0; j < currentName.Length; j++)
+                {
+                    if (currentName[j] == '*')
+                    {
+                        sumDamage *= 2.0;
+                    }
+                    else if (currentName[j] == '/')
+                    {
+                        sumDamage /= 2.0;
+                    }
+                }
+
+                if (!demons.ContainsKey(currentName))
+                {
+                    demons[currentName] = new List<double>();
+                }
+                demons[currentName].Add(sumHealth);
+                demons[currentName].Add(sumDamage);
+                ///Console.WriteLine(sumHealth);
+                //Console.WriteLine(sumDamage);
+            }
+
+            foreach (var demon in demons.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{demon.Key} - {demon.Value[0]} health, {demon.Value[1]:f2} damage");
+            }
+        }
+    }
+}
---
+++ Regular Expressions/SoftUni Bar Income/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z][a-z]+)%.*?<(\w+)>.*?\|(\d+)\|.*?(\d+\.?\d*)\$";
+            
+
+            string input = Console.ReadLine();
+
+            double totalPrice = 0;
+
+            while (input != "end of shift")
+            {
+                Regex regex = new Regex(pattern);
+
+                var validOrder = regex.Match(input);
+                if (validOrder.Length > 0)
+                {
+                    string customerName = validOrder.Groups[1].Value;
+                    string product = validOrder.Groups[2].Value;
+                    int count = int.Parse(validOrder.Groups[3].Value);
+                    double price = double.Parse(validOrder.Groups[4].Value);
+                    double currentPrice = count * 1.0 * price;
+                    totalPrice += currentPrice;
+
+                    Console.WriteLine($"{customerName}: {product} - {currentPrice:f2}");
+                }
+                
+
+                
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {totalPrice:f2}");
+        }
+    }
+}
