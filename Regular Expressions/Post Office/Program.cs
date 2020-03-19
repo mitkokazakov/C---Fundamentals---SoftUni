b25d3421d1c076e46cf76bd32d518763914f313b
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-19 16:39:35 +0200

    New task added

---
+++ Regular Expressions/Post Office/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+
+            string firstPart = input[0];
+            string secondPart = input[1];
+            string thirdPart = input[2];
+
+            string[] thirdPartArg = thirdPart.Split();
+
+            string firstPattern = @"([#$%*&])([A-Z]+)\1";
+            string secondPattern = @"([0-9]+[0-9]+):([0-9]+[0-9]+)";
+
+            Regex regex1 = new Regex(firstPattern);
+
+            var matches1 = regex1.Match(firstPart);
+            string capitalLetters = matches1.Groups[2].Value;
+
+            Regex regex2 = new Regex(secondPattern);
+
+            var matches2 = regex2.Matches(secondPart);
+
+            for (int i = 0; i < capitalLetters.Length; i++)
+            {
+                int currentLetter = capitalLetters[i];
+                int lenOfWord = 0;
+
+                foreach (Match digits in matches2)
+                {
+                    int currentDigit = int.Parse(digits.Groups[1].Value);
+
+                    if (currentDigit == currentLetter)
+                    {
+                        lenOfWord = int.Parse(digits.Groups[2].Value);
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < thirdPartArg.Length; j++)
+                {
+                    string currentWord = thirdPartArg[j];
+
+                    if (currentWord.Length == lenOfWord+1 && currentWord[0] == capitalLetters[i])
+                    {
+                        Console.WriteLine(currentWord);
+                        break;
+                    }
+                }
+            }
+
+            //Console.WriteLine(capitalLetters);
+        }
+    }
+}
---
+++ Regular Expressions/Santa's Secret Helper/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            List<string> goodChildren = new List<string>();
+
+            string input = Console.ReadLine();
+            string pattern = @"@([A-Za-z]+)[^@:\->!]*?!([G,N]+)!";
+
+            //string decryptedMessage = String.Empty;
+            
+            while (input != "end")
+            {
+                StringBuilder decryptedMessage = new StringBuilder();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    decryptedMessage.Append (Convert.ToChar(input[i] - key));
+                }
+
+                Regex regex = new Regex(pattern);
+                var matches = regex.Matches(decryptedMessage.ToString());
+
+                foreach  (Match kid in matches)
+                {
+                    if (kid.Groups[2].Value == "G")
+                    {
+                        goodChildren.Add(kid.Groups[1].Value);
+                    }
+                }
+
+                //Console.WriteLine(decryptedMessage);
+                input = Console.ReadLine();
+                
+            }
+
+            foreach (var kid in goodChildren)
+            {
+                Console.WriteLine(kid);
+            }
+        }
+    }
+}
