0a88f516e85df0ace55cde28e4a464ddd41b65b5
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-23 13:40:25 +0200

    Exam prep

---
+++ Final Exam - 03 August 2019 Group 2/Battle Manager/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Battle_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int[]> battle = new Dictionary<string, int[]>();
+            string commands = Console.ReadLine();
+
+            while (commands != "Results")
+            {
+                string[] commandArgs = commands.Split(":");
+                string mainCommand = commandArgs[0];
+
+                if (mainCommand == "Add")
+                {
+                    string name = commandArgs[1];
+                    int health = int.Parse(commandArgs[2]);
+                    int energy = int.Parse(commandArgs[3]);
+
+                    if (!battle.ContainsKey(name))
+                    {
+                        battle[name] = new int[2];
+                        battle[name][0] = health;
+                        battle[name][1] = energy;
+                    }
+                    else
+                    {
+                        battle[name][0] += health;
+                    }
+
+                }
+                else if (mainCommand == "Attack")
+                {
+                    string attackerName = commandArgs[1];
+                    string defenderName = commandArgs[2];
+                    int damage = int.Parse(commandArgs[3]);
+
+                    if (battle.ContainsKey(attackerName) && battle.ContainsKey(defenderName))
+                    {
+                        battle[defenderName][0] -= damage;
+
+                        if (battle[defenderName][0] <= 0)
+                        {
+                            battle.Remove(defenderName);
+                            Console.WriteLine($"{defenderName} was disqualified!");
+                        }
+
+                        battle[attackerName][1] -= 1;
+
+                        if (battle[attackerName][1] <= 0)
+                        {
+                            battle.Remove(attackerName);
+                            Console.WriteLine($"{attackerName} was disqualified!");
+                        }
+                    }
+
+                }
+                else if (mainCommand == "Delete")
+                {
+                    string username = commandArgs[1];
+
+                    if (username == "All")
+                    {
+                        battle.Clear();
+                    }
+
+                    else if (battle.ContainsKey(username))
+                    {
+                        battle.Remove(username);
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine($"People count: {battle.Keys.Count}");
+
+            foreach (var user in battle.OrderByDescending(x => x.Value[0]).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key} - {user.Value[0]} - {user.Value[1]}");
+            }
+        }
+    }
+}
---
+++ Final Exam - 03 August 2019 Group 2/String Manipulator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace String_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Done")
+            {
+                string[] commandArgs = commands.Split();
+                string mainCommand = commandArgs[0];
+
+                if (mainCommand == "Change")
+                {
+                    string oldChar = commandArgs[1];
+                    string newChar = commandArgs[2];
+
+                    while (text.Contains(oldChar))
+                    {
+                        text = text.Replace(oldChar, newChar);
+                    }
+
+                    Console.WriteLine(text);
+                }
+                else if (mainCommand == "Includes")
+                {
+                    string stringToContain = commandArgs[1];
+
+                    if (text.Contains(stringToContain))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (mainCommand == "End")
+                {
+                    string endString = commandArgs[1];
+
+                    bool result = text.EndsWith(endString);
+
+                    Console.WriteLine(result.ToString());
+                }
+                else if (mainCommand == "Uppercase")
+                {
+                    text = text.ToUpper();
+                    Console.WriteLine(text);
+                }
+                else if (mainCommand == "FindIndex")
+                {
+                    string wantedChar = commandArgs[1];
+                    int index = text.IndexOf(wantedChar);
+                    Console.WriteLine(index);
+                }
+                else if (mainCommand == "Cut")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+
+                    string savedString = text.Substring(startIndex,endIndex);
+                    text = savedString;
+                    Console.WriteLine(text);
+                }
+
+                commands = Console.ReadLine();
+            }
+        }
+    }
+}
