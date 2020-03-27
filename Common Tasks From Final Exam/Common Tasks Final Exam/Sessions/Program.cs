c61b08b3db87c1dc89ca314fbe5548a96567d123
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-27 14:02:29 +0200

    New exam

---
+++ Common Tasks From Final Exam/Common Tasks Final Exam/Sessions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> racersInfo = new Dictionary<string, List<string>>();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "END")
+            {
+                string[] commandArgs = commands.Split("->");
+                string mainCommand = commandArgs[0];
+
+                if (mainCommand == "Add")
+                {
+                    string road = commandArgs[1];
+                    string racer = commandArgs[2];
+
+                    if (!racersInfo.ContainsKey(road))
+                    {
+                        racersInfo[road] = new List<string>();
+                    }
+                    racersInfo[road].Add(racer);
+                }
+                else if (mainCommand == "Move")
+                {
+                    string currentRoad = commandArgs[1];
+                    string racer = commandArgs[2];
+                    string nextRoad = commandArgs[3];
+
+                    if (racersInfo[currentRoad].Contains(racer))
+                    {
+                        racersInfo[nextRoad].Add(racer);
+                        racersInfo[currentRoad].Remove(racer);
+                    }
+                }
+                else if (mainCommand == "Close")
+                {
+                    string road = commandArgs[1];
+                    if (racersInfo.ContainsKey(road))
+                    {
+                        racersInfo.Remove(road);
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine("Practice sessions:");
+            foreach (var road in racersInfo.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{road.Key}");
+
+                foreach (var racer in road.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
