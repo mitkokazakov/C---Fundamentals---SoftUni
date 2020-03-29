17b3a8a6c39492142f6ff4f0dc820b2efe30c849
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-29 11:13:25 +0300

    New tasks have been added to Array exercise

---
+++ Arrays/01.Train/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countWagons = int.Parse(Console.ReadLine());
+            int[] Wagons = new int[countWagons];
+            int sum = 0;
+
+            for (int i = 0; i < countWagons; i++)
+            {
+                Wagons[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            foreach (int people in Wagons)
+            {
+                Console.Write(people + " ");
+                sum += people;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Arrays/02.Common Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            foreach (string firstArrElements in firstArray)
+            {
+                foreach (string secondArrElements in secondArray)
+                {
+                    if (firstArrElements == secondArrElements)
+                    {
+                        Console.Write(firstArrElements + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
