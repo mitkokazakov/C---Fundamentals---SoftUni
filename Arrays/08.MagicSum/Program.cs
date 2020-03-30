160611aa9b8cf29152fc8978c7b467e4c83a0aa6
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-03-30 19:19:45 +0300

    New task added

---
+++ Arrays/08.MagicSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace _08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                for (int j = i+1; j < Numbers.Length; j++)
+                {
+                    if (Numbers[i] + Numbers[j] == magicNumber)
+                    {
+                        Console.WriteLine(Numbers[i] + " " + Numbers[j]);
+                    }
+                }
+            }
+        }
+    }
+}
