dcc236148633be7407ed2a0a1cb22e92fa9ce048
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-03 19:43:50 +0300

    Last task for today

---
+++ Associative Arrays/Largest 3 Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+
+            Console.WriteLine(String.Join(" ", numbers));
+
+        }
+    }
+}
