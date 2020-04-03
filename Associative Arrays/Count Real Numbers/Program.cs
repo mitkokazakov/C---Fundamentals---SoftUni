c3ac8e54fcfecd333b79f14323b13cc2b472a04e
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-03 19:42:54 +0300

    New project

---
+++ Associative Arrays/Count Real Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            SortedDictionary<string, int> numOfOccur = new SortedDictionary<string, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numOfOccur.ContainsKey(numbers[i]))
+                {
+                    numOfOccur[numbers[i]]++;
+                }
+                else
+                {
+                    numOfOccur[numbers[i]] = 1;
+                }
+            }
+
+            foreach (var number in numOfOccur)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
