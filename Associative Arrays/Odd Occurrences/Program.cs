582d4f3842a7d49923be685bb64cd98e0035d636
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-05 16:06:24 +0300

    New task

---
+++ Associative Arrays/Odd Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            Dictionary<string, int> oddOccur = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string lowerCaseWord = word.ToLower();
+                if (oddOccur.ContainsKey(lowerCaseWord))
+                {
+                    oddOccur[lowerCaseWord]++;
+                }
+                else
+                {
+                    oddOccur[lowerCaseWord] = 1;
+                }
+            }
+
+            foreach (var word in oddOccur)
+            {
+                if (word.Value % 2 !=0)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
