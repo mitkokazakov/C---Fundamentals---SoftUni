7d3fdc845ac020ec2dc65581a518067eb8f9472b
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-06 20:36:20 +0300

    New task

---
+++ Associative Arrays/Word Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
