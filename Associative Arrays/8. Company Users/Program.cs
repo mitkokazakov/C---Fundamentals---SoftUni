aff46199a6bd1367fe76d998a567a14078dc38c5
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-09 19:35:16 +0300

    New task added

---
+++ Associative Arrays/8. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _8._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> company = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] companyInfo = input.Split(" -> ");
+                string companyName = companyInfo[0];
+                string companyID = companyInfo[1];
+
+                if (!company.ContainsKey(companyName))
+                {
+                    company[companyName] = new List<string>();
+                }
+
+                if (!company[companyName].Contains(companyID))
+                {
+                    company[companyName].Add(companyID);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var singleCompany in company.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(singleCompany.Key);
+
+                foreach (var employeeID in singleCompany.Value)
+                {
+                    Console.WriteLine($"-- {employeeID}");
+                }
+            }
+        }
+    }
+}
