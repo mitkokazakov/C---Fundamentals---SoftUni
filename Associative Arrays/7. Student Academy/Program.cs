82d6705eb37eb07895945a21ce9282d8ae0f0b21
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-09 19:34:13 +0300

    New project

---
+++ Associative Arrays/7. Student Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> academy = new Dictionary<string, List<double>>();
+
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rows; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!academy.ContainsKey(name))
+                {
+                    academy[name] = new List<double>();
+                }
+
+                academy[name].Add(grade);
+            }
+
+            foreach (var student in academy.Where(x => x.Value.Average() >= 4.50).OrderByDescending(y => y.Value.Average()))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
