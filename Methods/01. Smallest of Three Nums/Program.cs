0b37c4fcec28272a5cdabc44cc5b852c3a533b56
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-15 16:57:10 +0300

    New task

---
+++ Methods/01. Smallest of Three Nums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Nums
+{
+    class Program
+    {
+        static void SmallestNum()
+        {
+            int number = 0;
+            int smallestNum = int.MaxValue;
+
+            for (int i = 0; i < 3; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number < smallestNum)
+                {
+                    smallestNum = number;
+                }
+            }
+
+            Console.WriteLine(smallestNum);
+        }
+
+        static void Main(string[] args)
+        {
+            SmallestNum();
+
+            
+        }
+
+        
+    }
+}
