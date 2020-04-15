e7a22ee45dd4e1322820b05ec7f77cda33e3f97c
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-15 16:56:34 +0300

    New task from Methods has been added

---
+++ Methods/01. DataTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._DataTypes
+{
+    class Program
+    { 
+
+        static void DetermineType( int intValue)
+        {
+            
+            int result = intValue * 2;
+            Console.WriteLine(result);
+        }
+
+        static void DetermineType( double doubleValue)
+        {
+            
+            double result = doubleValue * 1.5;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static void DetermineType(string stringValue)
+        {
+            Console.WriteLine("$" + stringValue + "$");
+        }
+
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string stringValue = Console.ReadLine();
+            if (dataType == "int")
+            {
+                int integerValue = int.Parse(stringValue);
+                DetermineType(integerValue);
+            }
+            else if (dataType == "real")
+            {
+                double doubleValue = double.Parse(stringValue);
+                DetermineType(doubleValue);
+            }
+            else if (dataType == "string")
+            {
+                DetermineType(stringValue);
+            }
+
+        }
+    }
+}
