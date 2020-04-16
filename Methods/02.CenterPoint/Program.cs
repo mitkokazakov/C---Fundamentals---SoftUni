419724e7f053ee034bef818ad547b6a00563d9c5
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-16 22:19:19 +0300

    New task

---
+++ Methods/02.CenterPoint/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.CenterPoint
+{
+    class Program
+    {
+        static double DetermineClosestPoint(double x1,double y1)
+        {
+            double line = Math.Sqrt((x1 * x1) + (y1*y1));
+            return line;
+        }
+
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double firstLine = DetermineClosestPoint(x1, y1);
+            double secondLine = DetermineClosestPoint(x2, y2);
+
+            if (firstLine > secondLine)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else if (secondLine > firstLine)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (firstLine == secondLine)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+    }
+}
