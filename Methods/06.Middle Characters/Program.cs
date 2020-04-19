fda5007e68f5c7798c0a5d0804ffe15d4f4bc3d2
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-19 17:42:22 +0300

    New task

---
+++ Methods/06.Middle Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.Middle_Characters
+{
+    class Program
+    {
+        static void PrintMidChar(string someString)
+        {
+            int lenght = someString.Length;
+
+            if (lenght % 2 == 0)
+            {
+                Console.WriteLine($"{someString[(lenght / 2) - 1]}{someString[lenght / 2]}");
+            }
+            else
+            {
+                Console.WriteLine(someString[lenght/2]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            PrintMidChar(input);
+        }
+    }
+}
