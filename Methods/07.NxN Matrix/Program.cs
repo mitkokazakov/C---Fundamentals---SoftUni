a396f0228bc348be2c7aebb13e8bbb4600e805f6
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-21 17:58:06 +0300

    New task

---
+++ Methods/07.NxN Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07.NxN_Matrix
+{
+    class Program
+    {
+        static void PrintMatrix(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    Console.Write(count + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            PrintMatrix(N);
+
+        }
+    }
+}
