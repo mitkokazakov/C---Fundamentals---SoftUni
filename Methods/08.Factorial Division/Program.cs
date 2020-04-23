25fdb956188947debb40cae82fb97bf61b506b5d
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-23 18:25:07 +0300

    New task

---
+++ Methods/08.Factorial Division/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _08.Factorial_Division
+{
+    class Program
+    {
+        static double FirstFactoriel(double firstNum)
+        {
+            double factoriel = 1;
+
+            for (int i = 1; i <= firstNum ; i++)
+            {
+                factoriel *= i;
+            }
+
+            return factoriel;
+        }
+
+        static double SecondFactoriel(double secondNum)
+        {
+            double factoriel = 1;
+
+            for (int i = 1; i <= secondNum; i++)
+            {
+                factoriel *= i;
+            }
+
+            return factoriel;
+        }
+
+
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double firstFactoriel = FirstFactoriel(firstNum);
+            double secondFactoriel = SecondFactoriel(secondNum);
+
+            
+                Console.WriteLine($"{firstFactoriel / secondFactoriel:f2}");
+            
+            
+
+        }
+    }
+}
