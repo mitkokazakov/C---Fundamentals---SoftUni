de454c5a50523d0f08e9b1b56e2af83371e14085
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-23 18:25:54 +0300

    New task

---
+++ Methods/09.Palindrome Integers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _09.Palindrome_Integers
+{
+    class Program
+    {
+        static void PrintPalindrome(string input)
+        {
+            string newString = "";
+
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                newString += input[i];
+            }
+
+            int originalNum = int.Parse(input);
+            int reversedNum = int.Parse(newString);
+
+            if (originalNum == reversedNum)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                
+                PrintPalindrome(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
