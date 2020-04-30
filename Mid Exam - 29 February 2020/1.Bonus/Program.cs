1e0424d05a77457c7ab2cf49876d07fd17d66a9c
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-04-30 22:44:59 +0300

    The easiest task from the mid exam

---
+++ Mid Exam - 29 February 2020/1.Bonus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1.Bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countStudents = int.Parse(Console.ReadLine());
+            int countLectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+            int maxBonus = 0;
+            int bestAttend = 0;
+
+            for (int i = 0; i < countStudents; i++)
+            {
+                int attEachStudent = int.Parse(Console.ReadLine());
+
+                double currentBonus = Math.Ceiling(((attEachStudent * 1.0 / countLectures * 1.0) * (5 + additionalBonus * 1.0)));
+                int integerBonus = Convert.ToInt32(currentBonus);
+                if (integerBonus >= maxBonus)
+                {
+                    maxBonus = integerBonus;
+                    bestAttend = attEachStudent;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {maxBonus}.");
+            Console.WriteLine($"The student has attended {bestAttend} lectures.");
+        }
+    }
+}
