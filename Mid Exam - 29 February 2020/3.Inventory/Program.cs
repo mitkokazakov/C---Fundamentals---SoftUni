e313a375a22db1d5cffcd3f0c741e7c00939ece4
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-05-04 19:03:23 +0300

    Last task from the exam

---
+++ Mid Exam - 29 February 2020/3.Inventory/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3.Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> journal = Console.ReadLine().Split(", ").ToList();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Craft!")
+            {
+                string[] commandArg = commands.Split(" - ");
+                string mainCommand = commandArg[0];
+
+                if (mainCommand == "Collect")
+                {
+                    string item = commandArg[1];
+
+                    if (!journal.Contains(item))
+                    {
+                        journal.Add(item);
+                    }
+                }
+                else if (mainCommand == "Drop")
+                {
+                    string item = commandArg[1];
+
+                    if (journal.Contains(item))
+                    {
+                        journal.Remove(item);
+                    }
+                }
+                else if (mainCommand == "Combine Items")
+                {
+                    string[] items = commandArg[1].Split(":");
+                    string oldItem = items[0];
+                    string newItem = items[1];
+
+                    if (journal.Contains(oldItem))
+                    {
+                        int indexToAdd = journal.IndexOf(oldItem);
+
+                        if (indexToAdd == journal.Count-1)
+                        {
+                            journal.Add(newItem);
+                        }
+                        else
+                        {
+                            journal.Insert(indexToAdd+1, newItem);
+                        }
+                    }
+                }
+                else if (mainCommand == "Renew")
+                {
+                    string item = commandArg[1];
+
+                    if (journal.Contains(item))
+                    {
+                        journal.Remove(item);
+                        journal.Add(item);
+                    }
+                }
+
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(", ", journal));
+        }
+    }
+}
