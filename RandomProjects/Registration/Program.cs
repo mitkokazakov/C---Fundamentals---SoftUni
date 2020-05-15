a26752a405204473401f2432a7c459edc5fc9b3f
mitkokazakov <dimitark38@gmail.com>
2020-05-15 20:57:08 +0300

    New project

---
+++ RandomProjects/Registration/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, User> regForm = new Dictionary<string, User>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] userInfo = input.Split();
+                string town = userInfo[0];
+                string name = userInfo[1];
+                string gender = userInfo[2];
+                int age = int.Parse(userInfo[3]);
+
+                User user = new User(name, gender, age);
+
+                if (!regForm.ContainsKey(town))
+                {
+                    regForm[town] = user;
+                }
+
+                if (regForm[town].Name == name)
+                {
+                    User existUser = regForm[town];
+                    existUser.ChangeYears(age);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var user in regForm)
+            {
+                Console.WriteLine($"{user.Value.Name} is from {user.Key} --- {user.Value.Gender} --- {user.Value.Age}!");
+            }
+        }
+    }
+}
---
+++ RandomProjects/Registration/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registration
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string gender, int age)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age;
+        }
+
+        public void ChangeYears(int age)
+        {
+            this.Age = age;
+        }
+    }
+}
