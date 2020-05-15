738db3e918179c1d30799ad1813aae96bd0ce8c0
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-15 21:17:02 +0300

    Test

---
+++ RandomProjects/NewSntaxSwitch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NewSntaxSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = 2;
+
+            var result = operation switch
+            {
+    1 => "Case 1",  
+    2 => "Case 2",  
+    3 => "Case 3",  
+    4 => "Case 4",  
+    _ => "No case availabe"
+};
+    }
+}
+}
